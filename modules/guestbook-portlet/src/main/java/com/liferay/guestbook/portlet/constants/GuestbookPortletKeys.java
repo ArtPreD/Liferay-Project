eca2556000e87098a5fc048d99deddd6c5251ac4
Artem Predvoditelev <a.predvodytelev@aimprosoft.com>
2019-01-15 15:15:08 +0200

    initial project version

---
+++ modules/guestbook-portlet/src/main/java/com/liferay/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,15 @@
+package com.liferay.guestbook.portlet.constants;
+
+/**
+ * @author predvodytelev
+ */
+public class GuestbookPortletKeys {
+
+	public static final String GUESTBOOK =
+			"com_liferay_guestbook_portlet_GuestbookPortlet";
+	public static final String DISPLAY_NAME_GUESTBOOK = "Guestbook Portlet";
+	public static final String GUESTBOOK_ADMIN =
+			"com_liferay_guestbook_portlet_GuestbookAdminPortlet";
+	public static final String DISPLAY_NAME_GUESTBOOK_ADMIN = "Guestbook Administration";
+
+}
